e6216188b8ddfe9e1db605b166369bc3e822c042
Yotam Achrak <yotam2585@gmail.com>
2020-10-19 21:14:37 +0300

    chgange DB

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 var cors = require('cors');
 
-mongoose.connect('mongodb://localhost:27017/codeTest', {
+mongoose.connect('mongodb://localhost:27017/codeTest3', {
 	autoReconnect: true,
 	reconnectTries: 60,
 	reconnectInterval: 10000
